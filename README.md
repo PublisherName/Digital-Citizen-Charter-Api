4cad549140fe40423a28b8629771e2db6591c153
publishername <62213479+PublisherName@users.noreply.github.com>
2024-03-29 16:28:48 +0545

    Initial Setup

---
+++ README.md
@@ -0,0 +1,29 @@
+# Digital Citizen Charter (DCC)
+
+Digital Citizen Charter ( डिजिटल नागरिक वडापत्र ) for Municipalities and government offices.
+
+## Introduction
+
+Digital Citizen Charter (DCC) is a digital version of the Citizen Charter. It is a public declaration of the commitments of the Municipalities and government offices to provide services to the citizens in a time-bound manner. It is a tool for public accountability and transparency in the administration. It is a platform for the citizens to know the services provided by the Municipalities and government offices, the time taken to provide the services, the procedure to avail the services, the fees to be paid for the services, the documents required to avail the services, the officer responsible for providing the services, the time taken to redress the grievances, etc.
+
+
+## Objective
+
+## Installation
+
+```python3 -m venv .venv```
+
+```source .venv/bin/activate```
+    
+```pip install -r requirements.txt```
+
+## Migration
+
+```python manage.py makemigrations```
+
+```python manage.py migrate```
+
+## Usage
+
+```python manage.py runserver```
+
---
+++ root/urls.py
@@ -0,0 +1,45 @@
+"""
+URL configuration for root project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions, routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+admin.site.site_title = "Digital Citizen Charter (DCC) administration"
+admin.site.site_header = "Digital Citizen Charter (DCC)"
+admin.site.index_title = "Site administration"
+admin.site.description = "Digital Citizen Charter ( डिजिटल नागरिक वडापत्र ) for Municipalities and government offices."
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title=admin.site.site_header + " Api",
+      default_version='v1',
+      description=admin.site.description,
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    ]
