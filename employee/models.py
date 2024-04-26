518b58e94aa8862344b72abb9fef8f68b1fd3311
publishername <62213479+PublisherName@users.noreply.github.com>
2024-04-26 16:23:52 +0545

    Employee Image Upload Through Url

--- employee/models.py
+++ employee/models.py
@@ -1,18 +1,12 @@
-"""This file contains the model for the Employee"""
-
 from django.core.exceptions import ValidationError
 from django.core.files.storage import default_storage
 from django.db import models
 
 from organization.models import Department, Designation, Organization
-from root.utils import UploadToPathAndRename
+from root.utils import UploadToPathAndRename, download_image_from_url
 
 
 class Employee(models.Model):
-    """
-    Employee model represents an employee in the system.
-    """
-
     name = models.CharField(max_length=200)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
@@ -23,18 +17,37 @@ class Employee(models.Model):
     profile_picture = models.ImageField(
         upload_to=UploadToPathAndRename("profile_pictures"), blank=True, null=True
     )
+    profile_picture_url = models.URLField(blank=True, null=True)
     is_available = models.BooleanField(default=True)
 
     def clean(self):
-        """
-        Overriding the clean method to validate the designation.
-        """
+        self.validate_designation()
+        self.download_profile_picture()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        try:
+            old_employee = Employee.objects.get(pk=self.pk)
+            if (
+                old_employee.profile_picture
+                and old_employee.profile_picture != self.profile_picture
+                and default_storage.exists(old_employee.profile_picture.name)
+            ):
+                default_storage.delete(old_employee.profile_picture.name)
+        except Employee.DoesNotExist:
+            pass
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.profile_picture and default_storage.exists(self.profile_picture.name):
+            default_storage.delete(self.profile_picture.name)
+        super().delete(*args, **kwargs)
+
+    def validate_designation(self):
         if self.department.organization != self.organization:
             raise ValidationError("Designation's department must match the selected organization.")
-
         if self.designation.department != self.department:
             raise ValidationError("Designation's must match the selected department.")
-
         if not self.designation.allow_multiple_employees and (
             Employee.objects.filter(designation=self.designation)
             .exclude(pk=self.pk if self.pk else None)
@@ -44,22 +57,11 @@ class Employee(models.Model):
                 f"The employee has already been assigned to the {self.designation} role."
             )
 
-    def save(self, *args, **kwargs):
-        """
-        Overriding the save method to set the organization and department based on the designation.
-        """
-        self.full_clean()
-        super().save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        """
-        Overriding the delete method to delete the profile picture from the storage.
-        """
-
-        if self.profile_picture and default_storage.exists(self.profile_picture.name):
-            default_storage.delete(self.profile_picture.name)
-
-        super().delete(*args, **kwargs)
+    def download_profile_picture(self):
+        if self.profile_picture_url:
+            image_file = download_image_from_url(self.profile_picture_url, f"{self.name}.jpg")
+            self.profile_picture.save(f"{self.name}.jpg", image_file, save=False)
+            self.profile_picture_url = None
 
     def __str__(self):
         return str(self.name)
